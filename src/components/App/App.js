fd1464ed9b901460c692a1705d122b8cc62e1669
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-05-13 14:17:12 +0200

    Add redux - App/List/Hero

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List';
-import {pageContents, listData} from '../../data/dataStore';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+    lists: PropTypes.PropTypes.array,
+  }
+
   render() {
+    const {lists} = this.props;
     return (
-        <main className={styles.component}>
-          <h1 className={styles.title}>{pageContents.title}</h1>
-          <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-          <List {...listData} />
-        </main>
-    )
+
+      <main className={styles.component}>
+        <h1 className={styles.title}>{this.props.title}</h1>
+        <h2 className={styles.subtitle}>{this.props.subtitle}</h2>
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))}
+      </main>
+    );
   }
 }
 export default App;
